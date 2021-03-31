1786f793ad9e560d33c36e46b6d027cfca64116b
sc0Vu <alk03073135@gmail.com>
2021-03-31 18:48:48 +0800

    Fix mainnet subgraph change: - fix lon staking address - fix traded token

--- src/helper.ts
+++ src/helper.ts
@@ -1,19 +1,19 @@
 /* eslint-disable prefer-const */
-import { BigInt, BigDecimal, ethereum, Address } from '@graphprotocol/graph-ts'
+import { BigInt, BigDecimal, ethereum, Address, Bytes } from '@graphprotocol/graph-ts'
 import { log } from '@graphprotocol/graph-ts'
 import { LonStaking } from "../generated/LonStaking/LonStaking"
 import { StakedDayData, StakedTotal, StakedChange, BuyBackTotal } from "../generated/schema"
 
-export const STAKING_ADDRESS = '0x413ecce5d56204962090eef1dead4c0a247e289b'
+export const STAKING_ADDRESS = '0xf88506b0f1d30056b9e5580668d5875b9cd30f23'
 export const LON_ADDRESS = '0x0000000000095413afc295d19edeb1ad7b71c952'
 export let ZERO = BigInt.fromI32(0)
 export let ZERO_BD = BigDecimal.fromString('0')
 export let ONE = BigInt.fromI32(1)
 export let ONE_BD = BigDecimal.fromString('1')
 export let LonStakingContract = LonStaking.bind(Address.fromString(STAKING_ADDRESS))
-export let ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
-export let ETH_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
-export let WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+export let ZERO_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
+export let ETH_ADDRESS = Address.fromString('0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE')
+export let WETH_ADDRESS = Address.fromString("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
 
 export function updateStakedData(event: ethereum.Event): void {
   let stakedChange = StakedChange.load(event.transaction.hash.toHex())
@@ -56,6 +56,6 @@ export function updateStakedData(event: ethereum.Event): void {
   stakedDayData.save()
 }
 
-export let isETH = (assetAddr: string): boolean => {
-  return (assetAddr === ZERO_ADDRESS) || (assetAddr === ETH_ADDRESS) || (assetAddr === WETH_ADDRESS)
+export let isETH = (assetAddr: Bytes): boolean => {
+  return (assetAddr == ZERO_ADDRESS) || (assetAddr == ETH_ADDRESS) || (assetAddr == WETH_ADDRESS)
 }
