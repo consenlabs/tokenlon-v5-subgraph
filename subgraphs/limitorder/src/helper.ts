d5edcf3cc3d476eb3fd795fb6f29f818f7a90c5d
Irara <irara.chen@token.im>
2023-08-16 12:08:13 +0800

    bug: When collecting TradedToken Entity, WETH assetAddr is replaced with Mainnet address
    When collecting TradedToken Entity, if the assetAddr is detected to be of the WETH type,
    it is currently being universally replaced with the Mainnet WETH address. Instead,
    the intention is to replace it with the WETH address specific to that network.

--- subgraphs/limitorder/src/helper.ts
+++ subgraphs/limitorder/src/helper.ts
@@ -1,27 +1,35 @@
 /* eslint-disable prefer-const */
-import { BigInt, BigDecimal, ethereum, Address, Bytes } from '@graphprotocol/graph-ts'
+import { BigInt, BigDecimal, ethereum, Address, Bytes, dataSource, log } from '@graphprotocol/graph-ts'
 import { ERC20 } from '../generated/LimitOrder/ERC20'
 import { ERC20Bytes } from '../generated/LimitOrder/ERC20Bytes'
 import { TradedToken, User } from '../generated/schema'
 
-export let ZERO = BigInt.fromI32(0)
-export let ZERO_BD = BigDecimal.fromString('0')
-export let ONE = BigInt.fromI32(1)
-export let ONE_BD = BigDecimal.fromString('1')
-export let ZERO_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
-export let ETH_ADDRESS = Address.fromString('0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE')
-export let WETH_ADDRESS = Address.fromString('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
+export const ZERO = BigInt.fromI32(0)
+export const ZERO_BD = BigDecimal.fromString('0')
+export const ONE = BigInt.fromI32(1)
+export const ONE_BD = BigDecimal.fromString('1')
+export const ZERO_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
+export const ETH_ADDRESS = Address.fromString('0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE')
+export const WETH_MAINNET = Address.fromString('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
+export const WETH_ARBITRUM = Address.fromString('0x82aF49447D8a07e3bd95BD0d56f35241523fBab1')
+export const WETH_GOERLI = Address.fromString('0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6')
 
-export let isETH = (assetAddr: Address): boolean => {
-  return assetAddr == ZERO_ADDRESS || assetAddr == ETH_ADDRESS || assetAddr == WETH_ADDRESS
+export const checkETH = (assetAddr: Address): Address => {
+  // Supported Networks: https://thegraph.com/docs/en/developing/supported-networks/
+  const networkName = dataSource.network()
+  // In the Subgraph handler, using the === statement always results in false.
+  // Please use the == statement for comparison.
+  const wethAddress = networkName == 'mainnet' ? WETH_MAINNET : networkName == 'arbitrum-one' ? WETH_ARBITRUM : networkName == 'goerli' ? WETH_GOERLI : ETH_ADDRESS
+
+  log.info('The current network is {}, and the obtained WETH address is {}.', [networkName, wethAddress.toHex()])
+
+  return assetAddr == ZERO_ADDRESS || assetAddr == ETH_ADDRESS || assetAddr == WETH_MAINNET || assetAddr == WETH_ARBITRUM || assetAddr == WETH_GOERLI ? wethAddress : assetAddr
 }
 
 export const addTradedToken = (tokenAddrBytes: Bytes, startDate: BigInt): TradedToken | null => {
   const tokenAddr: Address = Address.fromBytes(tokenAddrBytes)
-  let tokenAddrStr = tokenAddr.toHex()
-  if (isETH(tokenAddr)) {
-    tokenAddrStr = WETH_ADDRESS.toHex()
-  }
+  const tokenAddrStr = checkETH(tokenAddr).toHex()
+
   // check whether token is in the traded token
   let tradedToken = TradedToken.load(tokenAddrStr)
   if (tradedToken == null) {
